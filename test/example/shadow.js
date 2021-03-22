d9a798a4fc53d3eae9f47f4a824cc02e1476a8fb
luonan.666 <luonan.666@bytedance.com>
2021-03-22 16:11:25 +0800

    add shadow 3

--- test/example/shadow.js
+++ test/example/shadow.js
@@ -1,17 +1,39 @@
-window.C2S_EXAMPLES['drawLine'] = function (ctx) {
+window.C2S_EXAMPLES['shadow'] = function (ctx) {
     ctx.save();
+    test3(ctx);
+    ctx.restore();
+};
+
+function test3(ctx) {
+    ctx.rect(100, 40, 200, 100);
+    ctx.fillStyle = 'lime';
+    ctx.shadowColor = '#898';
+    ctx.shadowBlur = 20;
+    ctx.shadowOffsetX = 20;
+    ctx.shadowOffsetY = 20;
+    ctx.fill();
+}
+
+function test1(ctx) {
     ctx.shadowColor = "black";
     ctx.shadowBlur = 6;
     ctx.shadowOffsetX = 6;
     ctx.shadowOffsetY = 6;
     ctx.shadowColor = "orange";
-    // ctx.strokeRect(25, 25, 200, 200);
-    // ctx.shadowColor = "green";
-    // ctx.strokeRect(50, 50, 200, 200);
-    // ctx.shadowColor = "blue";
-    // ctx.strokeRect(75, 75, 200, 200);
-    // ctx.shadowColor = "red";
-    // ctx.strokeRect(100, 100, 200, 200);  
+    ctx.strokeRect(25, 25, 200, 200);
+    ctx.shadowColor = "green";
+    ctx.strokeRect(50, 50, 200, 200);
+    ctx.shadowColor = "blue";
+    ctx.strokeRect(75, 75, 200, 200);
+    ctx.shadowColor = "red";
+    ctx.strokeRect(100, 100, 200, 200);      
+}
+
+function test2(ctx) {
+    ctx.shadowColor = "black";
+    ctx.shadowBlur = 6;
+    ctx.shadowOffsetX = 6;
+    ctx.shadowOffsetY = 6;
 
     ctx.shadowColor = "orange";
     ctx.fillStyle = "white";
@@ -44,6 +66,4 @@ window.C2S_EXAMPLES['drawLine'] = function (ctx) {
     ctx.rect(100, 100, 200, 200);
     ctx.fill();
     ctx.closePath();
-    ctx.restore();
-};
-
+}
